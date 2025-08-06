62a811889d94546ee874e54bf915b2430f8fefc3
AugustoNicacio <augustonicacio1@gmail.com>
2025-08-06 23:55:15 +0000

    modificação do codigo

--- xadrez.c
+++ xadrez.c
@@ -4,41 +4,27 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
+int main() {
 // Mover a Torre 5 casas para a direita
 for (int i = 0; i < 5; i++) {
     printf("Torre Direita\n"); //Imprime a direção do movimento
 }
-// Mover o Bispo 5 casas para cima 
-for (int i = 0; i < 5; i++) {
-    printf("Bispo para cima\n"); //Imprime a direção do movimento
-}
-    // Mover o Bispo 5 casas para direita
-for (int i = 0; i < 5; i++) {
-    printf("Bispo para a direita\n"); //Imprime a direção do movimento
-}
 
+// Mover o Bispo 5 casas Diagonal Cima e Direita 
+int passos_bispo = 0;
+    while (passos_bispo < 5) {
+    printf("Bispo Diagonal Cima e Direita\n"); //Imprime a direção do movimento
+    passos_bispo++;}
 
-printf("Mova a Rainha UMA casa para direita... \n");
-    int i = 1;
+// Mover a Rainha 8 casas para a esquerda
+    int passos_rainha = 0;
    
     do {
-        printf("%d\n", i);
-        i++;
-    } while (i <= 1);
-printf("Mova a Rainha DUAS casas para cima... \n");
-    do {
-        printf("%d\n", i);
-        i++;
-    } while (i <= 3);
-printf("Mova a Rainha CINCO casas para esquerda... \n");
-    do {
-        printf("%d\n", i);
-        i++;
-    } while (i <= 8);
+        printf("rainha esquerda\n"); //Imprime a direção do movimento
+        passos_rainha++;}
+    while (passos_rainha < 8);
+    
 
     return 0;
 }
