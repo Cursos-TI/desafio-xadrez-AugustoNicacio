e6108605733d3f68662ae2bad73ceaa171c9e72c
AugustoNicacio <augustonicacio1@gmail.com>
2025-08-06 23:08:06 +0000

    Xadrez Nivel 1

--- xadrez.c
+++ xadrez.c
@@ -8,25 +8,37 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+// Mover a Torre 5 casas para a direita
+for (int i = 0; i < 5; i++) {
+    printf("Torre Direita\n"); //Imprime a direção do movimento
+}
+// Mover o Bispo 5 casas para cima 
+for (int i = 0; i < 5; i++) {
+    printf("Bispo para cima\n"); //Imprime a direção do movimento
+}
+    // Mover o Bispo 5 casas para direita
+for (int i = 0; i < 5; i++) {
+    printf("Bispo para a direita\n"); //Imprime a direção do movimento
+}
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+printf("Mova a Rainha UMA casa para direita... \n");
+    int i = 1;
+   
+    do {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 1);
+printf("Mova a Rainha DUAS casas para cima... \n");
+    do {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 3);
+printf("Mova a Rainha CINCO casas para esquerda... \n");
+    do {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 8);
 
     return 0;
 }
